a02b605a3e5fe13b8d39be9557215c799a6dc194
jpablo02 <jfejs_5@hotmail.com>
2022-06-13 16:01:57 -0700

    4. Hello, puts4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- *main - prints "Programming is like building a multilingual puzzle
- *return: 0
+ *main - prints sentence "Programming is like building a multilingual puzzle"
+ *Return: 0 for correct value, nothing for incorrect value
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
