4c394dd9efb3acd107d61b70b5783f4e775ebe09
jpablo02 <jfejs_5@hotmail.com>
2022-06-13 20:48:55 -0700

    5. Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - print sent "with proper grammar, but the outcome is a piece of art,"
+ *Return: 0 for correct value, nothing for incorrect value
+ */
+
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
