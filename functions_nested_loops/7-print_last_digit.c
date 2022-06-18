303ca2879df1ef6acb632fd3d5e7b83d2b715b18
jpablo02 <jfejs_5@hotmail.com>
2022-06-17 20:54:02 -0700

    7. There are only 3 colors, 10 digits, and 7 notes; it

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *print_last_digit - Prints the last digit of a number
- * @c: The character to be checked
+ * @n: The number in question.
  *
  * Return: Value of the last digit
  */
