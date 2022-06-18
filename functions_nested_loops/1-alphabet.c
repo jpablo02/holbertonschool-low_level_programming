d09309c077c7bd78276f67b3d85280461667d489
jpablo02 <jfejs_5@hotmail.com>
2022-06-17 18:37:45 -0700

    2. 10 x alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet
+ * print_alphabet_x10 - print alphabet 10 times
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	int l;
+	int count = 0;
+	char letter;
 
-	for (l = 'a'; l <= 'z'; l++)
+	while (count++ <= 9)
 	{
-		_putchar(l);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
 
 
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+
+	_putchar('\n');
+}
+
+
+
