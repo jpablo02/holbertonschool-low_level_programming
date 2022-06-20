d5ca5f6be2b8ddbb0ac463172b6fce72fa82a5e7
jpablo02 <jfejs_5@hotmail.com>
2022-06-20 14:13:15 -0700

    3. The numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,16 @@
 #include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9.
+ * no return.
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 47; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
