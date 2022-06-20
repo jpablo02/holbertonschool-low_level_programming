2552afd9552e5fa8c8d183bdfd90a38270e7120e
jpablo02 <jfejs_5@hotmail.com>
2022-06-20 10:25:51 -0700

    0. isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _issuper - check parameter as an uppercase char
+ * @c: input char
+ * Return: 1 if uppercase char. 0 otherwise
+ * */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
