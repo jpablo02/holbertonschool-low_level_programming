f2e85b9a92940bbd64d29abbb8faf2da1657750e
jpablo02 <jfejs_5@hotmail.com>
2022-06-20 11:36:23 -0700

    1. isdigit2

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * _issuper - check if parameter is nn uppercase char
- * @c: input char
- * Return: 1 if uppercase char. 0 otherwise
+ * int _issuper(int c) - checks if parameter is an uppercase character
+ * @c: input character
+ * Return: 1 if is uppercase character. Returns 0 otherwise
  */
+
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
