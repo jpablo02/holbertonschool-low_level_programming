080cf86ce1881e00d0d8a5dd158b394f8bf31f95
jpablo02 <jfejs_5@hotmail.com>
2022-06-20 18:55:22 -0700

    9. Fizz-Buzz2

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 	printf("%d", n);
 	for (n = 2; n <= 100; n++)
 	{
-		if ((n % 3 = 0) && (n % 5 = 0))
+		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf(" FizzBuzz");
 		}
-		else if (n % 3 = 0)
+		else if (n % 3 == 0)
 		{
 			printf(" Fizz");
 		}
-		else if (n % 5 = 0)
+		else if (n % 5 == 1)
 		{
 			printf(" Buzz");
 		}
